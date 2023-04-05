8fcd08747bc9392d9b3dc9fd63ade5b22db7d5e3
ThiagoLRF <thiagoleandro.1702@gmail.com>
2023-04-04 22:47:36 -0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,20 @@ document.querySelector("#qtde").addEventListener("change", calcularOrcamento)
 document.querySelector("#js").addEventListener("change", calcularOrcamento)
 document.querySelector("#layout_sim").addEventListener("change", calcularOrcamento)
 document.querySelector("#layout_nao").addEventListener("change", calcularOrcamento)
+document.querySelector("#prazo").addEventListener("change", calcularOrcamento)
 
 function calcularOrcamento() {
-    let qtde = document.querySelector("#qtde").value
-    let preco = qtde * 100
-
-    let js = document.querySelector("#js").checked
-    if (js) preco *= 1.1
-
-    let layout = document.querySelector("#layout_sim").checked
-    if(layout) preco +=500
     
-    document.querySelector("#preco").innerHTML = "R$" + preco.toFixed(2)
+    let preco = qtde.value * 100
+    if (js.checked) preco *= 1.1
+    if(layout_sim.checked) preco +=500
+
+    let taxaDeUrgencia = preco * (1.1 - prazo.value * 0.1)
+    preco += taxaDeUrgencia
+
+    label_prazo.innerHTML = `Prazo (${prazo.value} semanas)`
+    
+    output.innerHTML = `R$  ${preco}.toFixed(2)`
 
 }
 
