30426f8ab3d842bdd8ca7ac636ccd0bd0df82be8
ThiagoLRF <thiagoleandro.1702@gmail.com>
2023-04-04 22:22:58 -0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,20 @@ console.log("calculando orçamento")
 
 document.querySelector("#qtde").addEventListener("change", calcularOrcamento)
 document.querySelector("#js").addEventListener("change", calcularOrcamento)
+document.querySelector("#layout_sim").addEventListener("change", calcularOrcamento)
+document.querySelector("#layout_nao").addEventListener("change", calcularOrcamento)
 
 function calcularOrcamento() {
     let qtde = document.querySelector("#qtde").value
     let preco = qtde * 100
-    
 
     let js = document.querySelector("#js").checked
+    if (js) preco *= 1.1
 
-    if (js == true) {
-        preco = preco + (preco * 10/100)
-    }
-
-    document.querySelector("#preco").innerHTML = "R$" + preco
+    let layout = document.querySelector("#layout_sim").checked
+    if(layout) preco +=500
+    
+    document.querySelector("#preco").innerHTML = "R$" + preco.toFixed(2)
 
 }
 
