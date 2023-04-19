208e485d3fe726dd6648a12253afb139609d5655
ThiagoLRF <thiagoleandro.1702@gmail.com>
2023-04-18 21:31:25 -0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,16 @@ document.querySelector("#prazo").addEventListener("change", calcularOrcamento)
 
 function calcularOrcamento() {
     
-    let preco = qtde.value * 100
-    if (js.checked) preco *= 1.1
-    if(layout_sim.checked) preco +=500
+    let preco = qtde.value * 10
+    if (js.checked) preco *= 1.2
+    if(layout_sim.checked) preco +=100
 
-    let taxaDeUrgencia = preco * (1.1 - prazo.value * 0.1)
+    let taxaDeUrgencia = preco * (1.2 + prazo.value * 0.3)
     preco += taxaDeUrgencia
 
-    label_prazo.innerHTML = `Prazo (${prazo.value} semanas)`
+    label_prazo.innerHTML = `Horas Por Dia (${prazo.value} Horas)`
     
-    output.innerHTML = `R$  ${preco}.toFixed(2)`
+    output.innerHTML = `R$  ${preco.toFixed(2)}`
 
 }
 
